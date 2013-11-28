88d0e7cd55b15cbc581e9c48b38f27373bab62f5
Dave Clayton <daclayton@libertyglobal.com>
2013-11-28 13:43:00 +0100

    Changed Facebook app key to Sci Solvers app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for facebook
-  provider :facebook, '283643648422021', 'e2d0b2983b53f3008b03671d7273b4b8', {:scope => 'email'}
- 
-  # If you want to also configure for additional login services, they would be configured here.
+  provider :facebook, '580645801988575', 'cb67b2d7c551013fae51380ba4e71dc0', {:scope => 'email'}
 end
