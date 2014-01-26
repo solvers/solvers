071623299e79ddfa7f1d496c936d0fef8bd1e681
Dave Clayton <davedx@gmail.com>
2014-01-26 16:21:14 +0100

    Added some stuff to run

--- configure.sh
+++ configure.sh
@@ -3,5 +3,11 @@
 #install meteor
 curl https://install.meteor.com | /bin/sh
 
+#install grunt-cli
+npm install -g grunt-cli
+
 #installing meteorite
 npm install -g meteorite
+
+#run meteor project
+cd app;mrt
