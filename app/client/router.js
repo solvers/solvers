24e47957453f6bf60300b31652800771890271c8
Richard Smith <rds45@cam.ac.uk>
2014-02-20 12:07:54 +0000

    add analytics

--- app/client/router.js
+++ app/client/router.js
@@ -6,74 +6,80 @@ Router.map(function () {
   // Projects
   this.route('home', {
     path: '/',
-    controller: HomeController
+    controller: HomeController,
+    after: function() { GAnalytics.pageview('/'); }
   });
 
   // Projects by tag
   this.route('tagged', {
     path: '/projects/tag/:_tag',
-    controller: HomeController
+    controller: HomeController,
+    after: function() { GAnalytics.pageview('/projects/tag/:_tag'); }
   });
 
   // New project
   this.route('newProject', {
-    path: '/projects/new' 
+    path: '/projects/new',
+    after: function() { GAnalytics.pageview('/projects/new'); }
   });
 
   // Show project
   this.route('showProject', {
     path: '/projects/:_id',
     before:
-      function() {
-        this.subscribe('projects').wait();
-        this.subscribe('comments').wait();
-        var project = Projects.findOne({_id: this.params._id});
-        if(!project) {
-          this.render('notFound');
-          this.stop();
-        }
-        Session.set('project', project);
-      },
-      after: function() {
-          if (!Session.get(this.params._id)) {
-              Meteor.call('addView', this.params._id, function(err) {
-                  if(err) {
-                      console.log(err.reason);
-                  }
-              });
-              Session.set(this.params._id, true);
-          }
+    function() {
+      this.subscribe('projects').wait();
+      this.subscribe('comments').wait();
+      var project = Projects.findOne({_id: this.params._id});
+      if(!project) {
+        this.render('notFound');
+        this.stop();
       }
+      Session.set('project', project);
+    },
+    after: function() {
+      GAnalytics.pageview('/projects/:_id');
+      if (!Session.get(this.params._id)) {
+        Meteor.call('addView', this.params._id, function(err) {
+          if(err) {
+            console.log(err.reason);
+          }
+        });
+        Session.set(this.params._id, true);
+      }
+    }
   });
 
   // Edit profile
   this.route('profile', {
     path: '/profile',
     before: [
-      function() {
-        this.subscribe('projects').wait();
-        this.subscribe('comments').wait();
-        Session.set('profileUser', Meteor.userId());
-      }
+    function() {
+      this.subscribe('projects').wait();
+      this.subscribe('comments').wait();
+      Session.set('profileUser', Meteor.userId());
+    }
     ],
     data: function() {
       return Meteor.user();
-    }
+    },
+    after: function() { GAnalytics.pageview('/profile'); }
   });
 
   // Show profile
   this.route('userCard', {
     path:'/profile/:_id',
     before: [
-      function() {
-        this.subscribe('projects').wait();
-        this.subscribe('comments').wait();
-        Session.set('profileUser', this.params._id);
-      }
+    function() {
+      this.subscribe('projects').wait();
+      this.subscribe('comments').wait();
+      Session.set('profileUser', this.params._id);
+    }
     ],
     data: function() {
       return Meteor.users.findOne({ _id: this.params._id });
-    }
+    },
+    after: function() { GAnalytics.pageview('/profile/:_id'); }
   });
 
   // Admin stats
@@ -81,23 +87,27 @@ Router.map(function () {
     path:'/stats',
     waitOn: function() {
       return [this.subscribe('projects'),
-              this.subscribe('comments')];
-    }
+      this.subscribe('comments')];
+    },
+    after: function() { GAnalytics.pageview('/stats'); }
   });
 
   // About
   this.route('about', {
-    path: '/about'
+    path: '/about',
+    after: function() { GAnalytics.pageview('/about'); }
   });
 
   // Contact
   this.route('contact', {
-    path: '/contact'
+    path: '/contact',
+    after: function() { GAnalytics.pageview('/contact'); }
   });
 
   // 404
   this.route('notFound', {
-    path: '*'
+    path: '*',
+    after: function() { GAnalytics.pageview('/404'); }
   });
 
 });
