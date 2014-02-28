086bd05e52b4933f3ce71ba6a191faca1afae7b6
Richard Smith <richardsmith404@gmail.com>
2014-02-28 12:16:23 +0000

    boost to 10s sleep

--- configure.sh
+++ configure.sh
@@ -12,5 +12,5 @@ npm install -g meteorite
 #run meteor project
 cd app;mrt --settings settings_test.json &
 
-#give it 3 seconds to load before testing
-sleep 3
+#give it 10 seconds to load before testing
+sleep 10
